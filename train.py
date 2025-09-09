b3e4f2c440053384d9892534a03a92453395b77a
chyoo92 <chyoo041792@gmail.com>
2025-09-09 17:26:24 +0900

    add gitignore

--- train.py
+++ train.py
@@ -1,1 +1,5 @@
-train code
+import argparse
+import os
+from train import Train_DISCoNet
+from get_segmentation import get_segmentation
+from convert_results import combine_results, convert_single_result
